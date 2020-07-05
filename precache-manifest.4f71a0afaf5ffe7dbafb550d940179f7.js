4d1cdd4b57065ed8c9dd88aad7f702dadaa6c06c
Spartan-ash <ashishhsingh84@gmail.com>
2020-07-05 20:33:44 +0530

    Updates

---
+++ precache-manifest.4f71a0afaf5ffe7dbafb550d940179f7.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = [
+  {
+    "revision": "42d289a4094c9985c329",
+    "url": "/ashish-website/static/css/main.34e1ed8a.chunk.css"
+  },
+  {
+    "revision": "42d289a4094c9985c329",
+    "url": "/ashish-website/static/js/main.cf9e65a0.chunk.js"
+  },
+  {
+    "revision": "f13e2b07f58fb9ae5fed",
+    "url": "/ashish-website/static/js/runtime~main.e042374a.js"
+  },
+  {
+    "revision": "3c83798da5d170872f0c",
+    "url": "/ashish-website/static/css/2.79e0902d.chunk.css"
+  },
+  {
+    "revision": "3c83798da5d170872f0c",
+    "url": "/ashish-website/static/js/2.7a068be7.chunk.js"
+  },
+  {
+    "revision": "0d4f14eb4ef931e99f7bd7cbea5ed1d0",
+    "url": "/ashish-website/static/media/code_brackets-512.0d4f14eb.webp"
+  },
+  {
+    "revision": "e25c72242f2d59b636921618222bdb7a",
+    "url": "/ashish-website/static/media/ashish.e25c7224.jpg"
+  },
+  {
+    "revision": "064dc5216deeb53a94bc2fd864f3f581",
+    "url": "/ashish-website/static/media/s.064dc521.png"
+  },
+  {
+    "revision": "ab897c434295cd4a0f8a6004b403d6dd",
+    "url": "/ashish-website/static/media/s1.ab897c43.png"
+  },
+  {
+    "revision": "1958bef565d61d9291c9c0182916654a",
+    "url": "/ashish-website/static/media/s2.1958bef5.png"
+  },
+  {
+    "revision": "3849aaf96c1edad77e469fc455caf308",
+    "url": "/ashish-website/static/media/s3.3849aaf9.png"
+  },
+  {
+    "revision": "71055c7825222af829525e503c4a645b",
+    "url": "/ashish-website/static/media/s4.71055c78.png"
+  },
+  {
+    "revision": "9db61938ae7c4758752848b980d18f3f",
+    "url": "/ashish-website/static/media/s5.9db61938.png"
+  },
+  {
+    "revision": "7e53955aec471d47cdfde8e3c192a449",
+    "url": "/ashish-website/static/media/i.7e53955a.png"
+  },
+  {
+    "revision": "721bc322482ef5415acd7c594dc578e8",
+    "url": "/ashish-website/static/media/i1.721bc322.png"
+  },
+  {
+    "revision": "94d94b35d834f7c123d17aece593b69d",
+    "url": "/ashish-website/static/media/i2.94d94b35.png"
+  },
+  {
+    "revision": "169973ce5eee62b2788ffce384309fb1",
+    "url": "/ashish-website/static/media/Ss.169973ce.png"
+  },
+  {
+    "revision": "f73a8f99637d7f252d7989597b7a998c",
+    "url": "/ashish-website/static/media/Ss1.f73a8f99.png"
+  },
+  {
+    "revision": "43961b0d5f35b853f704d5cc282871e0",
+    "url": "/ashish-website/static/media/Ss2.43961b0d.png"
+  },
+  {
+    "revision": "1146d601b9a1203b057205fd0c8e217b",
+    "url": "/ashish-website/static/media/pic.1146d601.png"
+  },
+  {
+    "revision": "28d61b728474e8f3c79f4e2400053fdb",
+    "url": "/ashish-website/static/media/pic1.28d61b72.png"
+  },
+  {
+    "revision": "f8ac579ae3f4d83463044952fc933bd3",
+    "url": "/ashish-website/static/media/pic2.f8ac579a.png"
+  },
+  {
+    "revision": "295a50307d2eebec296740b3fc8d9131",
+    "url": "/ashish-website/static/media/pic3.295a5030.png"
+  },
+  {
+    "revision": "c589e9a9121d6c47abb03cf7fb5ce377",
+    "url": "/ashish-website/static/media/sss.c589e9a9.png"
+  },
+  {
+    "revision": "e60d831167682e9d8a7479cab98230e2",
+    "url": "/ashish-website/static/media/sss1.e60d8311.png"
+  },
+  {
+    "revision": "7bc19c7bfd9abe53765a70d1c639e2c2",
+    "url": "/ashish-website/static/media/sss2.7bc19c7b.png"
+  },
+  {
+    "revision": "56319b2a5b6864122f291187bb8a4e68",
+    "url": "/ashish-website/static/media/ss1.56319b2a.png"
+  },
+  {
+    "revision": "45750faa43f49aaea05e9b526cba56eb",
+    "url": "/ashish-website/static/media/ss2.45750faa.png"
+  },
+  {
+    "revision": "caf73ada07c1bfd9d5f2cb255ac0d0ce",
+    "url": "/ashish-website/static/media/ss3.caf73ada.png"
+  },
+  {
+    "revision": "f87cb796a67b7758ab8e0863dc5b71fb",
+    "url": "/ashish-website/static/media/ss4.f87cb796.png"
+  },
+  {
+    "revision": "d2cf8f4bb6ec490ec6d008fb4464eef3",
+    "url": "/ashish-website/static/media/ss5.d2cf8f4b.png"
+  },
+  {
+    "revision": "b9888ac57b5b3a5ba1796f26510ca94a",
+    "url": "/ashish-website/static/media/MovieInfo.b9888ac5.png"
+  },
+  {
+    "revision": "672debb67f3c8c58389096a925158be9",
+    "url": "/ashish-website/static/media/photo-1542831371-29b0f74f9713.672debb6.jpg"
+  },
+  {
+    "revision": "2c2ae068be3b089e0a5b59abb1831550",
+    "url": "/ashish-website/static/media/ionicons.2c2ae068.eot"
+  },
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "/ashish-website/static/media/ionicons.05acfdb5.woff"
+  },
+  {
+    "revision": "24712f6c47821394fba7942fbb52c3b2",
+    "url": "/ashish-website/static/media/ionicons.24712f6c.ttf"
+  },
+  {
+    "revision": "621bd386841f74e0053cb8e67f8a0604",
+    "url": "/ashish-website/static/media/ionicons.621bd386.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/ashish-website/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/ashish-website/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/ashish-website/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/ashish-website/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/ashish-website/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "6647e9770d5d185cae614c17e15edcaf",
+    "url": "/ashish-website/index.html"
+  }
+];
